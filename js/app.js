7ff456ca8fddd490653db1cbaa0e7e9f4b7024b2
cryptomehedi <classicalmehedi@gmail.com>
2022-02-17 02:43:31 +0600

    text to number

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// *select all id 
 const incomeInput = document.querySelector('#income-input');
 const foodInput = document.querySelector('#food-input');
 const rentInput = document.querySelector('#rent-input');
@@ -6,3 +7,11 @@ const saveInput = document.querySelector('#save-input');
 const calculatorButton = document.querySelector('#calculator-button');
 const saveButton = document.querySelector('#save-button');
 const restValue = document.querySelector('#rest-balance');
+
+calculatorButton.addEventListener('click',function(){
+    // *text to number convert
+    const incomeInputNum = parseInt(incomeInput.value) ;
+    const foodInputNum = parseInt(foodInput.value);
+    const rentInputNum = parseInt(rentInput.value);
+    const clothesInputNum = parseInt(clothesInput.value);
+});
